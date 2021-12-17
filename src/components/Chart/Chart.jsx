bcfcff9c4b25dcd87c58523608d1db84cae7c6c3
Anthony Zamarro <azamarro@veracode.com>
2021-12-16 21:13:12 -0500

    starter code for adding chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,54 @@
+import { useState, useEffect } from "react";
+import { fetchDailyData } from '../../API';
+import { Line, Bar } from 'react-chartjs-2';
+
+// import styles from './Chart.modules.css';
+
 const Chart = () => {
-    return <h2></h2>
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            setDailyData(await fetchDailyData());
+        }
+
+        fetchApi();
+    }, []);
+
+    const lineChart = (
+        dailyData[0] ? (
+            <Line
+                data={{
+                    labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+                    datasets: [{
+                        data: dailyData.map((data) => data.confirmed),
+                        label: 'Infected',
+                        borderColor: '#3333ff',
+                        fill: true,
+                    }, {
+                        data: dailyData.map((data) => data.deaths),
+                        label: 'Deaths',
+                        borderColor: 'red',
+                        backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                        fill: true,
+                    }, {
+                        data: dailyData.map((data) => data.recovered),
+                        label: 'Recovered',
+                        borderColor: 'green',
+                        backgroundColor: 'rgba(0, 255, 0, 0.5)',
+                        fill: true,
+                    },
+                    ],
+                }}
+            />
+        ) : null
+    );
+
+    return (
+        <div className='container'>
+            {lineChart}
+        </div>
+    );
 }
 
 export default Chart;
