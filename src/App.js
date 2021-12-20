c9c44dab2c0c7407e9f24f21d50787d04528393f
Anthony Zamarro <azamarro@veracode.com>
2021-12-19 22:06:16 -0500

    add country picker component logic and style app for mobile

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
+import { useEffect, useState } from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import { fetchData } from './API';
 
 import styles from './App.module.css';
-import { useEffect, useState } from 'react';
+
+import covidImage from './images/image.png';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [dataObj, setDataObj] = useState();
+  const [country, setCountry] = useState('');
 
-  useEffect(async () => {
-    const data = await fetchData();
-    // console.log(data);
-    if (data) {
-      setDataObj(data);
-      setLoading(false);
+  useEffect(() => {
+    const fetchApi = async () => {
+      const data = await fetchData();
+      if (data) {
+        setDataObj(data);
+        setLoading(false);
+      }
     }
+    fetchApi();
   }, []);
+
+  const handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+    setCountry(country);
+    setDataObj(data);
+  }
+
   return (
     <div className={styles.container}>
+      <img src={covidImage} alt='covid-19' className={styles.img} />
       {
         loading ? <h1>Loading...</h1> :
           <>
             <Cards data={dataObj} />
-            <CountryPicker />
-            <Chart />
+            <CountryPicker handleCountryChange={handleCountryChange} />
+            <Chart data={dataObj} country={country} />
           </>
       }
     </div>
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -5,23 +5,29 @@ import styles from './CountryPicker.module.css';
 
 import { fetchCurrentCountries } from "../../API";
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
     const [allCountries, setAllCountries] = useState([]);
     useEffect(() => {
         const fetchCountries = async () => {
             const { countries } = await fetchCurrentCountries();
             setAllCountries(countries);
-            console.log(allCountries);
         }
 
         fetchCountries();
-    }, []);
+    }, [setAllCountries]);
+
     return (
-        <FormControl className={styles.FormControl}>
-            <NativeSelect>
+        <FormControl className={styles.cp__container}>
+            <NativeSelect defaultValue='' onChange={e => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
                 {allCountries.map((country, index) => {
                     return (
-                        <option value={country.name} key={index}>{country.name}</option>
+                        <option
+                            value={country.name}
+                            key={index}
+                        >
+                            {country.name}
+                        </option>
                     )
                 })}
             </NativeSelect>
