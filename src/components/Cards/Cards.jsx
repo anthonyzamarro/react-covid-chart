3197da444f559c254f4e221f81e864f0c604f4ea
Anthony Zamarro <azamarro@veracode.com>
2021-12-20 08:37:05 -0500

    refactor card component to keep it dry

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,39 +4,39 @@ import cx from 'classnames';
 import styles from './Cards.module.css';
 
 const Cards = ({ data: { deaths, confirmed, lastUpdate, recovered } }) => {
+    const deathsObj = { ...deaths, title: 'Deaths', id: 1, style: styles.deaths };
+    const confirmedObj = { ...confirmed, title: 'Infected', id: 2, style: styles.infected };
+    const recoveredObj = { ...recovered, title: 'Recovered', id: 3, style: styles.recovered };
     return (
         <div className="styles container">
             <Grid container spacing={3} justifyContent="center">
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">
-                            <CountUp start={0} end={confirmed.value} duration={2.5} separator=',' />
-                        </Typography>
-                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">
-                            <CountUp start={0} end={recovered.value} duration={2.5} separator=',' />
-                        </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
-                        <Typography variant="body2">Number of recoveries from COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography variant="h5">
-                            <CountUp start={0} end={deaths.value} duration={2.5} separator=',' />
-                        </Typography>
-                        <Typography color="textSecondary">REAL DATE</Typography>
-                        <Typography variant="body2">Number deaths caused by COVID-19</Typography>
-                    </CardContent>
-                </Grid>
+                {<>
+                    {
+                        [deathsObj, confirmedObj, recoveredObj].map(value => {
+                            return (
+                                <Grid key={value.id} item component={Card} xs={12} md={3} className={cx(styles.card, `${value.style}`)}>
+                                    <CardContent>
+                                        <Typography color="textSecondary" gutterBottom>{value.title}</Typography>
+                                        <Typography variant="h5">
+                                            <CountUp start={0} end={value.value} duration={2.5} separator=',' />
+                                        </Typography>
+                                        <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                                        <Typography variant="body2">Number of {
+                                            value.title === 'Deaths'
+                                                ? 'deaths caused by'
+                                                : value.title === 'Infected'
+                                                    ? 'active cases of'
+                                                    : value.title === 'Recovered'
+                                                        ? 'recoveries from'
+                                                        : null
+                                        }  COVID-19</Typography>
+                                    </CardContent>
+                                </Grid>
+                            )
+                        })
+                    }
+                </>
+                }
             </Grid>
         </div>
     )
